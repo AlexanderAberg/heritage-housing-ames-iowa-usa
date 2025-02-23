c7fc28604bd14c421dbecab9e77f524577d7ba3e
Alexander <166539943+AlexanderAberg@users.noreply.github.com>
2025-02-23 12:29:54 +0000

    Updated the page_summary

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -3,34 +3,20 @@ import streamlit as st
 def page_summary_body():
     st.header("Project Summary")
     st.info("""
-            🌻 **General info:**
-            
-            A primary school wants to teach children about five of the most common flowers in a more interactive way. 
-            They came up with the idea of a "flower-bingo," where children can upload photos 
-            (with help from their parents) into an app with a bingo tray.
-
-            The squares in the bingo tray will be marked when a photo of that flower is uploaded. 
-            To ensure that the flowers are being marked correctly, 
-            the school reached out and wanted an app that can predict flowers from photos.
-
-            As a data analyst, I’ve been tasked with creating a prediction tool for this app that achieves an accuracy of over 80%. 
-            To accomplish this, I’m going to build a machine learning classifier using convolutional neural networks (CNNs) 
-            to accurately classify flower species based on images.
+             **General info:**
+            The page goal is to be able to estimate the housing prices in Aimes based on different attributes including for example something obvious like size but also less obvious 
+            such as when the garage was built.
             """)
     
     st.success("""
-               🌹 **Dataset:**
+                **Dataset:**
+                The dataset is taken from https://www.kaggle.com/datasets/codeinstitute/housing-prices-data
                
-                The dataset used for this project consists of 29,360 images of five common flower species: dandelions, daisies, sunflowers, tulips, and roses. 
-                The images are classified into these categories, with multiple samples for each flower species. 
-                This dataset is publicly available and can be used for educational and research purposes.
-
-                All images in this dataset are licensed under the Creative Commons By-Attribution License.
                 [Licence](https://github.com/)
                """)
     
     st.warning("""
-               🌼 **Business requirements:**
+                **Business requirements:**
                 - The client requires analysis and visualization of the dataset.
                 - The client expects a machine learning model with an accuracy above 80%.
                """)
